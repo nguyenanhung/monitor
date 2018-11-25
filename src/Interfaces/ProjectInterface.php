141b9b107966bf8f41dd951e7eac6ba6819a7955
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-11-26 00:46:44 +0700

    Create Project

---
+++ src/Interfaces/ProjectInterface.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Date: 10/4/18
+ * Time: 14:55
+ */
+
+namespace nguyenanhung\Monitor\Interfaces;
+
+/**
+ * Interface ProjectInterface
+ *
+ * @package   nguyenanhung\Monitor\Interfaces
+ * @author    713uk13m <dev@nguyenanhung.com>
+ * @copyright 713uk13m <dev@nguyenanhung.com>
+ */
+interface ProjectInterface
+{
+    const VERSION             = '1.0.0';
+    const LAST_MODIFIED       = '2018-11-24';
+    const AUTHOR_NAME         = 'Hung Nguyen';
+    const AUTHOR_EMAIL        = 'dev@nguyenanhung.com';
+    const PROJECT_NAME        = 'Monitor Connector';
+    const TIMEZONE            = 'Asia/Ho_Chi_Minh';
+    const REQUEST_TIMEOUT     = 60;
+    const EXIT_SUCCESS        = 0; // no errors
+    const EXIT_ERROR          = 1; // generic error
+    const EXIT_CONFIG         = 3; // configuration error
+    const EXIT_UNKNOWN_FILE   = 4; // file not found
+    const EXIT_UNKNOWN_CLASS  = 5; // unknown class
+    const EXIT_UNKNOWN_METHOD = 6; // unknown class member
+    const EXIT_USER_INPUT     = 7; // invalid user input
+    const EXIT_DATABASE       = 8; // database error
+    const EXIT__AUTO_MIN      = 9; // lowest automatically-assigned error code
+    const EXIT__AUTO_MAX      = 125; // highest automatically-assigned error code
+    const USE_BENCHMARK       = FALSE;
+    const USE_DEBUG           = FALSE;
+
+    /**
+     * Hàm lấy thông tin phiên bản Package
+     *
+     * @author  : 713uk13m <dev@nguyenanhung.com>
+     * @time    : 10/13/18 15:12
+     *
+     * @return mixed|string Current Project Version, VD: 0.1.0
+     */
+    public function getVersion();
+}
---
+++ src/Repository/config/slack_config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Project monitor.
+ * Created by PhpStorm.
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Date: 11/26/18
+ * Time: 00:45
+ */
+return [
+    'slack_messages' => [
+        'incoming_url' => ''
+    ]
+];
