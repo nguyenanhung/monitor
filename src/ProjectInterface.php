5bfac13c5a7b28476f06032a1a869f55b47a48cf
nguyenanhung <info@nguyenanhung.com>
2020-10-02 23:02:32 +0700

    Release version 2.0.0

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\Monitor;
  */
 interface ProjectInterface
 {
-    const VERSION       = '1.0.15';
-    const LAST_MODIFIED = '2020-09-03';
+    const VERSION       = '2.0.0';
+    const LAST_MODIFIED = '2020-10-02';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     const PROJECT_NAME  = 'Monitor Connector';
